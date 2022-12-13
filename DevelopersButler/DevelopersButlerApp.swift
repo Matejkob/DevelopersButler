27ff4895bceca2f75519c8bd26bf20e2f224cc0b
Mateusz Bąk <44930823+Matejkob@users.noreply.github.com>
2022-12-13 21:03:11 +0100

    Initial Commit

---
+++ DevelopersButler/DevelopersButlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersButlerApp.swift
+//  DevelopersButler
+//
+//  Created by Mateusz Bąk on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersButlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
